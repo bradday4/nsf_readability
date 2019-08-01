2a131528535128baeb23967e92d1dd4dddbbe2f2
bday4 <bradday4@gmail.com>
2019-08-01 09:10:53 -0500

    linting

--- basextest.py
+++ basextest.py
@@ -1,5 +1,6 @@
+# -*- coding: utf-8 -*-
 '''
-testing script for querying baseX database and generating dataframe of abstracts
+Query baseX database and generating dataframe of abstracts
 '''
 # %%
 
@@ -15,12 +16,15 @@ SESSION = BaseXClient.Session('localhost', 1984, 'admin', 'admin')
 try:
     QRY = SESSION.query(
         """element root { (for $x_1 in db:text("nsf_awards", "Standard Grant")/
-        parent::*:Value/parent::*:AwardInstrument/parent::*:Award order by 
-        $x_1/descendant::*:AwardEffectiveDate empty least return 
-        if(((normalize-space(((: xs:string?, true :) $x_1/descendant::*:AbstractNarration)) != "") 
-        and (normalize-space(((: xs:string?, true :) $x_1/descendant::*:AwardEffectiveDate)) != ""))) 
-        then element award { (($x_1/descendant::*:AbstractNarration, $x_1/descendant::*:AwardEffectiveDate)) } 
-        else ()) }"""
+        parent::*:Value/parent::*:AwardInstrument/parent::*:Award order by
+         $x_1/descendant::*:AwardEffectiveDate empty least return
+         if(((normalize-space(((: xs:string?, true :)
+         $x_1/descendant::*:AbstractNarration)) != "")
+         and (normalize-space(((: xs:string?, true :)
+         $x_1/descendant::*:AwardEffectiveDate)) != "")))
+         then element award { (($x_1/descendant::*:AbstractNarration,
+         $x_1/descendant::*:AwardEffectiveDate)) }
+         else ()) }"""
         )
 # run query on database
     RESPONSE = QRY.execute()
@@ -43,14 +47,18 @@ DF = pd.DataFrame({'effDate': EFFDATE,
 # convert from text to date
 DF['effDate'] = pd.to_datetime(DF['effDate'])
 # drop duplicate abstracts from dataset
-DF.drop_duplicates(subset='abstract',keep='first', inplace=True)
+DF.drop_duplicates(subset='abstract', keep='first', inplace=True)
 # %%
 with open('cleaned_abstracts.txt', 'w', encoding='utf-8') as text_file:
     for abstract in DF['abstract']:
         text_file.write(abstract+'\n\n')
-STAT = []
+# STAT_FLESCH = []
+# STAT_NDC = []
+STAT_CONSENSUS = []
 for index, row in DF.iterrows():
-    STAT.append(textstat.flesch_reading_ease(row[1]))
+    # STAT_FLESCH.append(textstat.flesch_reading_ease(row[1]))
+    # STAT_NDC.append(textstat.dale_chall_readability_score(row[1]))
+    STAT_CONSENSUS.append(textstat.text_standard(row[1], float_output=True))
 
 
 # %%
